398a2877468b3a928b46d0d16451a7ccb14df759
Miguel Christian Sanchez <mcsanche@ucsd.edu>
2021-10-23 21:01:44 -0700

    added img and audio src change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,30 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+function hornChange(event) {
+  let image = document.querySelector("#expose > img");
+  let audio = document.querySelector('audio');
+  let selectedOption = event.target.value;
+  switch(selectedOption) {
+    case 'party-horn':
+      image.src = "assets/images/party-horn.svg";
+      audio.src = "assets/audio/party-horn.mp3";
+    break;
+    case 'car-horn':
+      image.src = "assets/images/car-horn.svg";
+      audio.src = "assets/audio/car-horn.mp3";
+    break;
+    case 'air-horn':
+      image.src = "assets/images/air-horn.svg";
+      audio.src = "assets/audio/air-horn.mp3";
+    break;
+    default:
+      image.src = "assets/images/no-images.png";
+      audio.src = "";
+  }
+}
+
 function init() {
-  // TODO
+  const hornSelect = document.getElementById('horn-select');
+  hornSelect.addEventListener('change', hornChange);
 }
