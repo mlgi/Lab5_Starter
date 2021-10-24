95446612f8206ab6665f239d39c5f108bb42d013
Miguel Christian Sanchez <mcsanche@ucsd.edu>
2021-10-23 21:23:37 -0700

    volume change and volume icon change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,9 +3,10 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function hornChange(event) {
-  let image = document.querySelector("#expose > img");
-  let audio = document.querySelector('audio');
+  const image = document.querySelector("#expose > img");
+  const audio = document.querySelector('audio');
   let selectedOption = event.target.value;
+
   switch(selectedOption) {
     case 'party-horn':
       image.src = "assets/images/party-horn.svg";
@@ -25,7 +26,28 @@ function hornChange(event) {
   }
 }
 
+function volumeChange(event) {
+  let audio = document.querySelector('audio');
+  let targetVolume = Number(event.target.value);
+  const volumeIcon = document.querySelector('#volume-controls > img');
+
+  audio.volume = targetVolume / 100.0;
+
+  if (targetVolume == 0) {
+    volumeIcon.src = 'assets/icons/volume-level-0.svg';
+  } else if (targetVolume >= 1 && targetVolume < 33) {
+    volumeIcon.src = 'assets/icons/volume-level-1.svg';
+  } else if(targetVolume >= 33 && targetVolume < 67) {
+    volumeIcon.src = 'assets/icons/volume-level-2.svg';
+  } else {
+    volumeIcon.src = 'assets/icons/volume-level-3.svg';
+  }
+}
+
 function init() {
   const hornSelect = document.getElementById('horn-select');
   hornSelect.addEventListener('change', hornChange);
+
+  const volumeControl = document.getElementById('volume');
+  volumeControl.addEventListener('input',volumeChange);
 }
